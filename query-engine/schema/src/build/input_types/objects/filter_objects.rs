94b15d012f28fc9d0c4459a8c60b23de41ded060
New Author Name <new.author.email@example.com>
2024-03-18 19:14:03 +0800

    feat: add uk is optional in unique_input

--- query-engine/schema/src/build/input_types/objects/filter_objects.rs
+++ query-engine/schema/src/build/input_types/objects/filter_objects.rs
@@ -150,12 +150,8 @@ pub(crate) fn where_unique_object_type(ctx: &'_ QuerySchema, model: Model) -> In
                 let name = sf.borrowed_name(&ctx.internal_data_model.schema);
                 let typ = map_scalar_input_type_for_field(ctx, sf);
                 let comment = f.borrowed_comment(&ctx.internal_data_model.schema);
-                // 主键为 required, 非主键为 optional
-                if sf.is_id() {
-                    simple_input_field(name, typ, None, comment)
-                } else {
-                    InputField::new(name.into(), vec![typ], None, false, comment)
-                }
+                // 主键和非主键为 optional
+                InputField::new(name.into(), vec![typ], None, false, comment)
             })
             .collect();
 
